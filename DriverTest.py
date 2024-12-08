fdcdb56aab6c1cc370981f9313abf3911faca556
Paakallo <paviveerar@gmail.com>
2024-12-08 15:45:23 +0100

    add updated test2

--- DriverTest.py
+++ DriverTest.py
@@ -7,7 +7,9 @@ import pyaudio
 import wave
 import numpy as np
 import matplotlib.pyplot as plt
-
+import random
+import os
+import csv
 
 sys.setrecursionlimit(10000)
 
@@ -79,6 +81,7 @@ class Test1:
         """
         self.dialog.destroy()
 
+
 class Test2:
     def __init__(self):
         pygame.init()
@@ -91,6 +94,7 @@ class Test2:
         self.RED = (255, 0, 0)
         self.GREEN = (0, 255, 0)
         self.BLUE = (0, 0, 255)
+        self.RANDOM = (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
 
         self.fs= 44100 #sampling rate
 
@@ -124,38 +128,44 @@ class Test2:
     def tutorial_menu(self):
         self.dialog = tk.Toplevel()
         self.dialog.title("Tutorial")
-        self.label = tk.Label(self.dialog, text=f"Do you want to go through a tutorial")
+        self.label = tk.Label(self.dialog, text="Do you want to go through a tutorial?")
         self.label.pack()
 
-        self.yes_button = tk.Button(self.dialog, text="Yes", command=self.tutorial())
+        self.yes_button = tk.Button(
+            self.dialog, text="Yes", command=lambda: [self.dialog.destroy(), self.tutorial()]
+        )
         self.yes_button.pack()
 
-        self.no_button = tk.Button(self.dialog, text="No", command=self.dialog.destroy())
+        self.no_button = tk.Button(self.dialog, text="No", command=self.dialog.destroy)
         self.no_button.pack()
+
+        self.dialog.wait_window()
 
 
     def tutorial(self):
         self.screen.fill(self.WHITE)
-        pygame.draw.rect(self.screen, self.RED, (350, 250, 100, 100))
+        pygame.draw.rect(self.screen, self.RANDOM, (350, 250, 100, 100))
         pygame.display.update()
 
-        init = False
         event_found = False
         while not event_found:
             events = pygame.event.get()
-            for event in events:
-
-                if not init:
-                    start_time= self.init_level(5)
-                    init = True
-
+            for event in events:          
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     event_found = True
                     break
+        self.screen.fill(self.BLACK)
+        
+        self.dialog = tk.Toplevel()
+        self.dialog.title("Tutorial")
+        
+        self.no_button = tk.Button(self.dialog, text="Continue", command=self.dialog.destroy)
+        self.no_button.pack()
 
+        self.dialog.wait_window()
 
     def init_level(self,duration:int,stimulus):
-        # wait 5 sec
+        # wait n sec
         self.cool_down(time.time(),duration)
 
         start_time = time.time()
@@ -163,9 +173,9 @@ class Test2:
         return start_time
 
 
-    def first_level(self):
+    def run_level(self,color,stimulus, duration,level):
         self.screen.fill(self.WHITE)
-        pygame.draw.rect(self.screen, self.RED, (350, 250, 100, 100))
+        pygame.draw.rect(self.screen, color, (random.randint(0, 800), random.randint(0, 600), 100, 100))
         pygame.display.update()
 
         init = False
@@ -176,69 +186,36 @@ class Test2:
             for event in events:
 
                 if not init:
-                    start_time= self.init_level(5,self.audio_stimulus1)
+                    start_time= self.init_level(duration,stimulus)
                     init = True
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     end_time = time.time()
                     reaction_time = end_time - start_time
-                    self.results.append(reaction_time)
+                    self.results.append({"test_no":2,"level":level,"time":reaction_time})
                     event_found = True
                     break
-            
+
+        self.show_result_dialog()
+
+
+    def first_level(self):
+        self.run_level(self.RED,self.audio_stimulus1,2,1)      
 
     def second_level(self):
-        self.screen.fill(self.WHITE)
-        pygame.draw.rect(self.screen, self.GREEN, (350, 250, 100, 100))
-        pygame.display.update()
-
-        init = False
-        start_time = 0
-        event_found = False
-        while not event_found:
-            events = pygame.event.get()
-            for event in events:
-                
-                if not init:
-                    start_time= self.init_level(3,self.audio_stimulus2)
-                    init = True
-
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    end_time = time.time()
-                    reaction_time = end_time - start_time
-                    self.results.append(reaction_time)
-                    event_found = True
-                    break
-
+        self.run_level(self.GREEN,self.audio_stimulus2,1,2)
 
     def third_level(self):
-        self.screen.fill(self.WHITE)
-        pygame.draw.rect(self.screen, self.BLUE, (350, 250, 100, 100))
-        pygame.display.update()
-
-        init = False
-        start_time = 0
-        event_found = False
-        while not event_found:
-            events = pygame.event.get()
-            for event in events:
-                
-                if not init:
-                    start_time= self.init_level(1,self.audio_stimulus2)
-                    init = True
-
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    end_time = time.time()
-                    reaction_time = end_time - start_time
-                    self.results.append(reaction_time)
-                    event_found = True
-                    break
+        self.run_level(self.BLUE,self.audio_stimulus2,0.75,3)
 
 
     def game_loop(self):
         first_check = False
         second_check = False
         third_check = False
+
+        self.tutorial_menu()
+        self.cool_down(time.time(),1)
 
         while True:
             if not first_check:
@@ -254,14 +231,56 @@ class Test2:
                 third_check = True
 
             if first_check and second_check and third_check:
+                self.save_results()
+                self.show_results()
                 pygame.quit()
                 break
     
-    def get_result(self):
-        return self.results
-    
+
+    def save_results(self):
+        
+        file_exists = os.path.isfile("test2.csv")
+        with open("test2.csv", mode='a', newline='') as csv_file:
+            fieldnames = self.results[0].keys()
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                  
+            if not file_exists:
+                writer.writeheader()
+  
+           
+            for data in self.results:
+                writer.writerow(data)
+
+
+    def show_result_dialog(self):
+        self.dialog = tk.Toplevel()
+        self.dialog.title("Test 1 Results")
+
+        for result in self.results:
+            label = tk.Label(self.dialog, text=f"Your reaction time is {result['time']:.3f} seconds.")
+            label.pack()
+
+        # Dodanie przycisku zamykającego
+        close_button = tk.Button(self.dialog, text="Close", command=self.dialog.destroy)
+        close_button.pack()
+
+        self.dialog.wait_window()    
 
     
+    def show_results(self):
+        result_times = []
+
+        for result in self.results:
+            result_times.append(result['time'])
+
+        plt.plot(result_times, label='Reaction Time (s)')
+        plt.xlabel('Trial')
+        plt.ylabel('Reaction Time (s)')
+        plt.title('Reaction Time Results')
+        plt.legend()
+        plt.show()
+
+
 class IntroductionDialog:
     def __init__(self, parent):
         self.parent = parent
